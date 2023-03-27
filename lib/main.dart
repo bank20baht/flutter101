1cd1119a91aa75b227510bcedd7ae03156e1f9cf
Nattapong <banker.z412@gmail.com>
2023-03-27 22:03:46 +0700

    learn how to create stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -25,44 +25,60 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Quote'),
       ),
-      body: Column(
-        children: [
-          Card(
-            margin: EdgeInsets.all(8),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  height: 300,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4),
-                    image: const DecorationImage(
-                      fit: BoxFit.cover,
-                      image: NetworkImage("https://cdn.pixabay.com/photo/2022/02/22/06/21/cat-7028109_960_720.png")
-                      )
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    'นายเองก็เป็นได้นะ Hero หน่ะ',
-                    style: TextStyle(
-                      fontSize: 20,
-                      
-                    ),),
-                ),
-                  
-                Container(
-                  alignment: const Alignment(1, 0),
-                  padding: const EdgeInsets.all(8.0),
-                  child: const Text(
-                    'all might',
-                    style: TextStyle(
-                      fontStyle: FontStyle.italic
-                    ),
-                    ),
-                ),
-              ],
+      body: ListView(
+        children: const [
+          QuoteCard("11111", "bank", "https://cdn.pixabay.com/photo/2022/02/22/06/21/cat-7028109_960_720.png"),
+          QuoteCard("22222", "bob", "https://cdn.pixabay.com/photo/2017/11/11/17/40/cat-2939726_960_720.jpg"),
+          QuoteCard("3333", "boy", "https://cdn.pixabay.com/photo/2019/05/21/07/11/cat-4218424_960_720.jpg"),
+        ],
+      ),
+    );
+  }
+}
+
+class QuoteCard extends StatelessWidget {
+  final String _text;
+  final String _author;
+  final String _imgURL;
+  const QuoteCard(this._text, this._author, this._imgURL, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(8),
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 300,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage('$_imgURL')
+                )
             ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              '"$_text"',
+              style: const TextStyle(
+                fontSize: 20,
+                
+              ),),
+          ),
+            
+          Container(
+            alignment: const Alignment(1, 0),
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              '$_author',
+              style: const TextStyle(
+                fontStyle: FontStyle.italic
+              ),
+              ),
           ),
         ],
       ),
